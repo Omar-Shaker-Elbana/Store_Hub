b283ff6a80374cc1b52dc9ca1875575fd8039970
Omar Shaker <omarshaker082@gmail.com>
2026-02-10 20:46:28 +0200

    fixed a typo in landing_page views

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    return render(request, 'landing/landing.html')
