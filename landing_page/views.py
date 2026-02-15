0916d22c62535fb86b177ead683043a7e39c5b43
mark1234720 <markissa45@gmail.com>
2026-02-15 02:53:41 +0200

    Fixing landing page conflicts and styling the login page

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'landing/landing_page.html')
+    return render(request, 'landing/index.html')
