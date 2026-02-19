da0deea9128515699bc3308892e306c9ed0c2ba7
Omar Shaker <omarshaker082@gmail.com>
2026-02-19 17:23:25 +0200

    Created models of products app and basic views of the app, also created needed models in the merchant interface app

--- merchant_interface/models.py
+++ merchant_interface/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Store(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    niche = models.CharField(max_length=100, null=True, blank=True)
+    inaguration_date = models.DateField(auto_now_add=True, blank=True, null=True)
+    picture = models.ImageField(upload_to='store_pics/', null=True, blank=True)
+    nationality = models.CharField(max_length=100, null=True, blank=True)
+    
+class Membership(models.Model):
+    ROLE_CHOICES = [
+    ('Owner', 'owner'),
+    ('Helper', 'helper'),
+    ('Manager', 'manager')
+    ]
+
+    WAGE_CHOICES = [
+    ('Salary', 'salary'),
+    ('Percentage', 'percentage')
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=7, choices=ROLE_CHOICES, default='helper')
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    join_date = models.DateField(null=True, blank=True)
+    wage_type = models.CharField(max_length=10, choices=WAGE_CHOICES, default='helper')
+    wage = models.DecimalField(null = True, blank = True, decimal_places=2, max_digits=10)
---
+++ products/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Product, Review, Spec
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'category',
+            'manufacturing_price',
+            'selling_price',
+            'stock'
+        ]
+
+class SpecForm(forms.ModelForm):
+    class Meta:
+        model = Spec
+        fields = ['name', 'value']
+        
+class Review_Form(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['stars', 'comment']
