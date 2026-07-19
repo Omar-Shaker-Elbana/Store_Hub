945673252b064d8df6d5e8f0d34e008113ba3aa3
Omar Shaker <omarshaker082@gmail.com>
2026-07-19 21:05:11 +0300

    added Sign uo with google

--- Online_Store/urls.py
+++ Online_Store/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
-    # path('accounts/', include('allauth.urls')),  # allauth handles login, logout, signup
+    path('accounts/', include('allauth.urls')),  # allauth handles login, logout, signup
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('notifications/', include('notifications.urls')),
